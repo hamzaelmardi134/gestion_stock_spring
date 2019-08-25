19d86a9298b277cc28d1a43a15c7db4c91932da8
Hamza1344 <hamzajobs134@gmail.com>
2019-08-25 22:35:40 +0100

    First Commit for the project GestionStock

---
+++ src/main/java/com/stock/mvc/Main.java
@@ -0,0 +1,27 @@
+package com.stock.mvc;
+
+import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import com.mysql.jdbc.Driver;
+import com.sun.xml.internal.ws.api.pipe.NextAction;
+
+public class Main {
+
+	public static void main(String[] args) throws SQLException {
+	
+		
+
+		
+
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,18 @@
+<%@page import="java.sql.DriverManager"%>
+<%@page import="java.sql.Connection"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+<h1>
+	Hello world!  
+</h1>
+
+
+
+<P>  The time on the server is ${serverTime}. </P>
+</body>
+</html>
